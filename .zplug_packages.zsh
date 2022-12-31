8cf06ba7d52b67ccaf3e62281eea6ed7e3791676
Christopher Masto <chris@masto.com>
2022-12-31 17:29:26 -0500

    Add autoswitch-virtualenv plugin

--- .zplug_packages.zsh
+++ .zplug_packages.zsh
@@ -2,3 +2,4 @@ zplug "zplug/zplug", hook-build:"zplug --self-manage"
 zplug "clvv/fasd", as:command, use:fasd
 zplug "b4b4r07/enhancd", use:init.sh
 zplug "g-plane/zsh-yarn-autocompletions"
+zplug "MichaelAquilina/zsh-autoswitch-virtualenv"
